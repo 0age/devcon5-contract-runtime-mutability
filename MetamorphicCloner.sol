febf21b79829780e523c27c611c4d933dac346ee
0age <37939117+0age@users.noreply.github.com>
2019-10-06 19:08:51 -0400

    Update MetamorphicCloner.sol

--- MetamorphicCloner.sol
+++ MetamorphicCloner.sol
@@ -1,45 +1,29 @@
 pragma solidity 0.5.11;
 
 
-contract TransientMetamorphicDeployer { 
-  // We'll store the creation code to deploy (temporarily).
-  bytes private _creationCodeToDeploy; 
+contract MetamorphicCloner { 
+  // We'll store the address to clone (temporarily).
+  address private _addressToCloneInStorageSlotZero; 
 
-  // The fallback function will return this creation code when called.
+  // The fallback function will return this address when called.
   function () external { 
-    // Move creation code from storage into memory.
-    bytes memory creationCode = _creationCodeToDeploy; 
-    
-    // Return using data offset (skip first 32-byte word) + size (first word).
-    assembly { return(add (32, creationCode), mload(creationCode)) }
-  } 
+    assembly { 
+      mstore(0, sload(0)) // Get the target address from storage slot 0.
+      return(0, 32) // Return the target address. 
+    }
+  }
 
-  function deployViaTransient(
-    bytes calldata creationCode
-  ) external returns (address metamorphic) { 
-    // Set the creation code in contract storage.
-    _creationCodeToDeploy = creationCode; 
+  function deployClone(address target) external returns (address clone) { 
+    // Set the target to clone in storage slot zero.
+    _addressToCloneInStorageSlotZero = target; 
 
     // Deploy the contract with fixed, non-deterministic creation code.
     assembly { 
-      mstore(0, 0x58593d59335afa3d59343d59593ef080601a573d81ed81803efd5bff00000000) 
-
-      let transient := create2(0, 0, 28, 0)
-      if iszero(transient) {
-        // "bubble up" any revert and pass along reason. 
-        returndatacopy(0, 0, returndatasize)
-        revert(0, returndatasize)
-      }
-
-      // Use hashed RLP encoding of deployer address + nonce to get deployed address.
-      mstore(0, or(
-        0xd694600000000000000000000000000000000000000001000000000000000000, 
-        shl(80, transient)
-      ))
-      metamorphic := keccak256(0, 23) //_solidity will mask upper dirty bits 
+      mstore(0, 0x5860203d593d335afa1551803b80928080933cf3000000000000000000000000)   
+	  clone := create2(0, 0, 20, 0) 
     }
 
-    // Clear out storage once we're done.
-    delete _creationCodeToDeploy;
+    // Clear out the storage slot once we're done.
+    delete _addressToCloneInStorageSlotZero;
   }
 }
